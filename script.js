fca83c8e2d767a4823de32fbea0f33561d00c797
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-27 19:51:54 +0530

    working

---
+++ script.js
@@ -0,0 +1,5 @@
+addEventListener("mousemove", (e) => {
+    document.documentElement.style.setProperty('--x', e.clientX + "px");
+    document.documentElement.style.setProperty('--y', e.clientY + "px");
+});
+
